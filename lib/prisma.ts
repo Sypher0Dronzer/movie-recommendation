07d7acf439c1ea034edd083a6e5b7b4e4fd4d733
Sypher0Dronzer <sohamsaha321@gmail.com>
2026-02-09 21:18:17 +0530

    import prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,6 @@
+import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
-import { PrismaClient } from '../src/generated/prisma/client'
+// import { PrismaClient } from '../src/generated/prisma/client'
 
 const prisma = new PrismaClient()
 
